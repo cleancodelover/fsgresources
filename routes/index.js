3e8037bef2feb562f97f991a617ed895287699be
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-06-25 19:03:47 +0100

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const about = require('../data/about.json');
-const books = require('../data/books.json')
+const about = require("../data/about.json");
+const books = require("../data/books.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FSG | Resources', about, books });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "FSG | Resources", about, books });
+});
+
+router.get("/index.html", function (req, res, next) {
+  res.redirect("/");
 });
 
 module.exports = router;
