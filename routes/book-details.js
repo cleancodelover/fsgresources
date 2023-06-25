d1d6d38e3eb818251556ba45f934553e942ecdb2
peter <peter@exarotech.com>
2023-06-25 18:10:21 +0100

    dynamic data

---
+++ routes/book-details.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const books = require('../data/books.json')
+
+/* GET home page. */
+router.get('/book-details/:title?', function(req, res, next) {
+  const book = books.find((item)=>item.book_title.toLocaleLowerCase()==req.params.title.toLocaleLowerCase());
+  console.log("Book title :>>>>>>>>>>>>>>", book);
+  res.render('book-details', { title: 'Book Details', book });
+});
+
+module.exports = router;
