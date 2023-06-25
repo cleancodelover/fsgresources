9b7b7ab974eba2e0999347ea62985cc7d0d7004e
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-06-25 19:02:57 +0100

    Update

--- routes/book-details.js
+++ routes/book-details.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const books = require('../data/books.json')
+const books = require("../data/books.json");
 
 /* GET home page. */
-router.get('/book-details/:title?', function(req, res, next) {
-  const book = books.find((item)=>item.book_title.toLocaleLowerCase()==req.params.title.toLocaleLowerCase());
+router.get("/book-details/:title?", function (req, res, next) {
+  const book = books.find(
+    (item) =>
+      item.book_title.toLocaleLowerCase() ==
+      req.params.title.toLocaleLowerCase()
+  );
   console.log("Book title :>>>>>>>>>>>>>>", book);
-  res.render('book-details', { title: 'Book Details', book });
+  res.render("book-details", { title: "Book Details", book });
 });
 
 module.exports = router;
