707af808dff467a2aae92e72f6b1065f3712d2c0
Verkyav Peter <peter.v@agtrinex.com>
2023-07-04 14:57:44 +0100

    added book

--- routes/book-details.js
+++ routes/book-details.js
@@ -4,6 +4,8 @@ const books = require("../data/books.json");
 
 /* GET home page. */
 router.get("/book-details/:title?", function (req, res, next) {
+  console.log("Books :>>>>>>>>>>>>>>>>", books);
+  console.log("Books :>>>>>>>>>>>>>>>>", req.params.title);
   const book = books.find(
     (item) =>
       item.book_title.toLocaleLowerCase() ==
